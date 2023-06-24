1762706418bbe48d1a497055a67320c8b54dfd6f
minhduc <minhductxqt@gmail.com>
2023-06-24 18:47:02 +0700

    setup project

---
+++ App.js
@@ -0,0 +1,29 @@
+import { KeyboardAvoidingView, Platform } from 'react-native'
+import { Provider } from 'react-redux'
+import { store } from './store'
+import HomeScreen from './screens/HomeScreen'
+import { SafeAreaProvider } from 'react-native-safe-area-context'
+import { NavigationContainer } from '@react-navigation/native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+
+const Stack = createNativeStackNavigator()
+
+export default function App() {
+  return (
+    <Provider store={store}>
+      <SafeAreaProvider>
+        <NavigationContainer>
+          <KeyboardAvoidingView
+            behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+            keyboardVerticalOffset={Platform.OS === 'ios' ? -64 : 0}
+            style={{ flex: 1 }}
+          >
+            <Stack.Navigator screenOptions={{ headerShown: false }}>
+              <Stack.Screen name='HomeScreen' component={HomeScreen} />
+            </Stack.Navigator>
+          </KeyboardAvoidingView>
+        </NavigationContainer>
+      </SafeAreaProvider>
+    </Provider>
+  )
+}
---
+++ screens/HomeScreen.js
@@ -0,0 +1,11 @@
+import { SafeAreaView } from 'react-native'
+import React from 'react'
+import { useDispatch } from 'react-redux'
+
+const HomeScreen = () => {
+  const dispatch = useDispatch()
+
+  return <SafeAreaView className='h-full bg-white'></SafeAreaView>
+}
+
+export default HomeScreen
